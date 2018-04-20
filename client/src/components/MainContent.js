c3dcf84dba51d4181e4b86e438f951111103cffb
Ted Shin <tdyshin@gmail.com>
2018-04-20 16:51:53 -0400

    Add comments and change variable names

--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -2,9 +2,10 @@ import { Button, Card, Input } from 'antd';
 import React, { Component } from 'react';
 const { TextArea } = Input;
 
-const highlightColor = '#FFFF00'; // yellow
-const noHighlightColor = '#FFFFFF'; // white
+const yellowColor = '#FFFF00'; // yellow
+const whiteColor = '#FFFFFF'; // white
 const grayColor = '#E8E8E8';
+const redColor = '#ff0000';
 
 const text = '절에 가서 도 닦는 것만 도가 아니야. 사람과 부대끼면서 도를 닦으라 그래 나는. 왜냐면 그래야 좋은 거 나쁜 거 보면서 깨달음을 얻거든. 나쁜 사람을 만나고 ‘저 사람은 원래 나쁜사람이구나’ 인정하면 내 마음이 편해져. 그런데 ‘에이 저놈 보기도 싫어’ 이러면 내 속이 나빠져. 병 걸린다고. 나쁜 사람에 대한 미움을 왜 내 마음에 두고 살아. 왜 내가 내 속을 썩여. 좋은 것만 자꾸 받아들여야 해. 좋은 것이 쌓이면 금이 되어버려. 그리고 마음이 금이 되면 꽃이 펴. 그 꽃은 건 지지 않는 꽃이야. 내가 마음에 심어두면 항상 펴 있어. 사람이 왜 좋은 줄 알아? 수시로 꽃이 필 수 있잖아.';
 
@@ -13,13 +14,14 @@ class MainContent extends Component {
     super(props);
 
     // create list of syllable block background colors. first one is yellow and rest are white
-    // const backgroundColorList = [highlightColor, ...Array(text.length - 1).fill(noHighlightColor)];
+    // const backgroundColorList = [yellowColor, ...Array(text.length - 1).fill(whiteColor)];
 
     this.state = {
       numKeystrokes: 0,
-
       timerStarted: false,
       textAreaValue: '',
+
+
       symbolIndex: 0,
       timeLeft: 60,
       wordsTyped: 0,
@@ -35,12 +37,14 @@ class MainContent extends Component {
 
   };
 
+  // start timer by using setInterval
   startTimer = () => {
     if (this.timer === 0) {
       this.timer = setInterval(this.countDown, 1000);
     }
   }
 
+  // update timer every second
   countDown = () => {
     // Remove one second, set state so a re-render happens.
     let timeLeft = this.state.timeLeft - 1;
@@ -54,8 +58,8 @@ class MainContent extends Component {
     }
   }
 
+  // handler for text area
   handleTextAreaChange = (event) => {
-
 
     // console.log(event.targe  t.value);
     // console.log(event.target.value.slice(-1));
@@ -63,43 +67,56 @@ class MainContent extends Component {
     if (!timerStarted) {
       this.startTimer();
       timerStarted = true;
-    }
-    const currentInput = event.target.value;
+    };
+
+    const entireTextAreaInput = event.target.value;
     let symbolIndex = this.state.symbolIndex;
     let wordsTyped = this.state.wordsTyped;
-    const currentlyTypedSymbol = currentInput.slice(-1);
+    // currentlyTypedSymbol is the symbol the user is current typing
+    const currentlyTypedSymbol = entireTextAreaInput.slice(-1);
     const correctMatchingSymbol = text[symbolIndex];
     // if typed symbol matches currently highlighted symbol, increment wordsTyped
     if (currentlyTypedSymbol === correctMatchingSymbol) {
       wordsTyped++;
+      symbolIndex++;
     }
-    console.log('###');
-    console.log('current input: ' + currentInput);
-    console.log(this.state.textAreaValue);
     const lastInput = this.state.textAreaValue;
+    console.log('###');
+    console.log('current input: ' + entireTextAreaInput);
+    console.log('last input: ' + lastInput);
     // this is when the user presses backspace and deletes one symbol
-    if (currentInput.length < lastInput.length) {
+    if (entireTextAreaInput.length < lastInput.length) {
       // move highlighted symbol back one
       symbolIndex--;
       if (currentlyTypedSymbol !== ' ') {
         wordsTyped--;
       }
-    } else if (currentInput.length > lastInput.length) {
-      symbolIndex++;
     }
+    // else if (entireTextAreaInput.length > lastInput.length) {
+    //   symbolIndex++;
+    // }
 
 
     // update states
     this.setState({
       timerStarted: timerStarted,
-      textAreaValue: currentInput,
+      textAreaValue: entireTextAreaInput,
       symbolIndex: symbolIndex,
       wordsTyped: wordsTyped
     });
   }
 
+  // handler for key press in text area
+  // used to keep track of number of key strokes
   keyPress = (e) => {
-    const numKeystrokes = this.state.numKeystrokes + 1;
+    console.log('keypress: ' + e.key);
+    const keypress = e.key;
+    let numKeystrokes;
+    if (keypress === 'Backspace') {
+      numKeystrokes = this.state.numKeystrokes - 1;
+    } else {
+      numKeystrokes = this.state.numKeystrokes + 1;
+    }
     this.setState({
       numKeystrokes: numKeystrokes
     });
@@ -112,21 +129,33 @@ class MainContent extends Component {
     const speed = this.state.speed;
     const accuracy = this.state.accuracy;
 
+    const textAreaValue = this.state.textAreaValue;
+    // console.log(textAreaValue);
+    const textAreaValueLength = textAreaValue.length;
+
     let textStyled = text.split('').map((e, i) => {
-      const symbolIndex = this.state.symbolIndex;
-      if (i === symbolIndex) {
+      // highlight the symbol that the user is on
+      if (i === textAreaValueLength - 1) {
+        <span id={ 'syllableBlock_' + i } key={ i } style={{ backgroundColor: yellowColor }}>{ e }</span>
+      }
+      else if (i >= textAreaValueLength) {
+        // color background white for chars ahead of user
         return (
-          <span id={ 'syllableBlock_' + i } key={ i } style={{ backgroundColor: highlightColor }}>{ e }</span>
+          <span id={ 'syllableBlock_' + i } key={ i } style={{ backgroundColor: whiteColor }}>{ e }</span>
         );
       }
-      else if (i <= symbolIndex) {
+      const correctCharMatch = textAreaValue[i];
+      // console.log("~~~");
+      // console.log('e: ' + e);
+      // console.log('correctCharMatch': correctCharMatch);
+      if (e === correctCharMatch) {
         return (
           <span id={ 'syllableBlock_' + i } key={ i } style={{ backgroundColor: grayColor }}>{ e }</span>
         );
       }
       else {
         return (
-          <span id={ 'syllableBlock_' + i } key={ i } style={{ backgroundColor: noHighlightColor }}>{ e }</span>
+          <span id={ 'syllableBlock_' + i } key={ i } style={{ backgroundColor: redColor }}>{ e }</span>
         );
       }
 
