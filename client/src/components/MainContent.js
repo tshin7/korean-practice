9a3ed8201fd8feaad572ddc5224fd40485bfbc48
Ted Shin <tdyshin@gmail.com>
2018-05-19 10:35:47 -0400

    Open modal when typing session is over and set proper values in modal

--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -4,10 +4,12 @@ const { TextArea } = Input;
 
 const yellowColor = '#FFFF00'; // yellow
 const whiteColor = '#FFFFFF'; // white
-const grayColor = '#E8E8E8';
+const grayColor = '#E8E8E8'; // gray
 
+// korean text for typing practice
 const text = '절에 가서 도 닦는 것만 도가 아니야. 사람과 부대끼면서 도를 닦으라 그래 나는. 왜냐면 그래야 좋은 거 나쁜 거 보면서 깨달음을 얻거든. 나쁜 사람을 만나고 ‘저 사람은 원래 나쁜사람이구나’ 인정하면 내 마음이 편해져. 그런데 ‘에이 저놈 보기도 싫어’ 이러면 내 속이 나빠져. 병 걸린다고. 나쁜 사람에 대한 미움을 왜 내 마음에 두고 살아. 왜 내가 내 속을 썩여. 좋은 것만 자꾸 받아들여야 해. 좋은 것이 쌓이면 금이 되어버려. 그리고 마음이 금이 되면 꽃이 펴. 그 꽃은 건 지지 않는 꽃이야. 내가 마음에 심어두면 항상 펴 있어. 사람이 왜 좋은 줄 알아? 수시로 꽃이 필 수 있잖아.';
-const timeGiven = 60;
+// total time given for typing practice
+const timeGiven = 5;
 
 class MainContent extends Component {
   constructor(props) {
@@ -22,9 +24,11 @@ class MainContent extends Component {
       modalVisible: false
     };
 
+    // timer for typing session
     this.timer = 0;
   }
 
+  // set cursor focus in textarea
   focus = () => {
     this.textInput.focus();
   }
@@ -35,7 +39,7 @@ class MainContent extends Component {
     this.focus();
   }
 
-  componentWillMount(){
+  componentWillMount() {
     document.addEventListener("keyup", this.handleDocKeyUp);
   };
 
@@ -45,6 +49,7 @@ class MainContent extends Component {
 
   // start timer by using setInterval
   startTimer = () => {
+    // setInterval on page load, when this.timer is set to 0
     if (this.timer === 0) {
       this.timer = setInterval(this.countDown, 1000);
     }
@@ -63,11 +68,15 @@ class MainContent extends Component {
     // Check if we're at zero.
     if (timeLeft === 0) {
       clearInterval(this.timer);
+      // show modal after timer hits 0
+      this.showModal();
     }
   }
 
   resetButton = () => {
+    // clear session timer
     clearInterval(this.timer);
+    // set session states to 0
     this.setState({
       timeLeft: 0,
       numKeystrokes: 0,
@@ -84,7 +93,9 @@ class MainContent extends Component {
       timerStarted = true;
     };
 
+    // entire text in textarea
     const entireTextAreaInput = event.target.value;
+    // index of symbol user is up to
     let symbolIndex = this.state.symbolIndex;
     // currentlyTypedSymbol is the symbol the user is current typing
     const currentlyTypedSymbol = entireTextAreaInput.slice(-1);
@@ -103,24 +114,28 @@ class MainContent extends Component {
     });
   }
 
+  // set modal visibliity to true
   showModal = () => {
     this.setState({
       modalVisible: true,
     });
   }
+
+  // set modal visiblity to false on OK click
   handleOk = (e) => {
     console.log(e);
     this.setState({
       modalVisible: false,
     });
   }
+
+  // set modal visiblity to false on Cancel click
   handleCancel = (e) => {
     console.log(e);
     this.setState({
       modalVisible: false,
     });
   }
-  rende
 
   render() {
     // index of symbol that user is currently up to
@@ -163,7 +178,7 @@ class MainContent extends Component {
 
     return (
       <div>
-        <div>Start typing to begin typing practice</div>
+        <div style={{ fontSize: '2em' }}>Start typing to begin</div>
         <Button onClick={this.resetButton}>Reset</Button>
         <div style={{ fontSize: '1.5em' }}>Time Left: { timeLeft }</div>
         <div style={{ fontSize: '1.5em' }}>Key Strokes: { numKeystrokes }</div>
@@ -177,18 +192,19 @@ class MainContent extends Component {
           onKeyUp={this.keyPress}
           onChange={this.handleTextAreaChange}
           ref={input => this.textInput = input }
+          /* Hide textarea off screen */
           style={{ fontSize: '2em', userSelect: 'none', position: 'absolute', left: '-999em' }}
         />
-        <Button type="primary" onClick={ this.showModal }>Open</Button>
+        { /* <Button type="primary" onClick={ this.showModal }>Open</Button> */ }
         <Modal
           title="Typing Summary"
           visible={this.state.modalVisible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
         >
-          <p>Keystrokes: </p>
-          <p>Words Typed: </p>
-          <p>WPM: </p>
+          <div style={{ fontSize: '1.5em' }}>Key Strokes: { numKeystrokes }</div>
+          <div style={{ fontSize: '1.5em' }}>Words Typed: { wordsTyped }</div>
+          <div style={{ fontSize: '1.5em' }}>Speed: { speed } WPM</div>
         </Modal>
       </div>
     );
