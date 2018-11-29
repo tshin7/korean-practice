cc75ec19f3333394a996a55bbf19874e71d4abaa
Theodore Shin <tdyshin@gmail.com>
2018-11-28 22:28:13 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
 
 import './App.css';
 
-import Head from './components/Head';
+import Navbar from './components/Navbar';
 import Main from './components/Main';
+// import Footer from './components/Footer';
 
 class App extends Component {
-
-  state = {
-    collapsed: false,
-  };
-
-  toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  }
-
   render() {
     return (
-      <Layout>
-        <Head collapsed={ this.state.collapsed } />
-        <Main collapsed={ this.state.collapsed } />
-      </Layout>
+      <div className="App">
+        <Navbar />
+        <Main />
+      </div>
     );
   }
 }
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Menu,
+} from 'semantic-ui-react'
+
+// Fonts from Fonts.js file
+import fonts from './Fonts';
+
+export default class NavBar extends Component {
+  state = {
+    activeMenuItem: 'home',
+  };
+
+  navbarButtonClicked = (event, { name }) => {
+    this.setState({ activeMenuItem: name });
+  };
+
+  render() {
+    // const {fonts} = this.props.fonts;
+    const {activeMenuItem} = this.state;
+    return (
+      <Container>
+        <Menu
+          fixed="top"
+          pointing
+          secondary
+          size="large"
+          style={{ backgroundColor: 'white' }}
+        >
+          <Menu.Item
+            as={Link}
+            header
+            to="/"
+            style={{ fontSize: '1.2em', fontFamily: `${fonts.main}, sans-serif` }}
+          >
+            Korean Typing Practice
+          </Menu.Item>
+          <Menu.Item
+            name="home"
+            active={activeMenuItem === 'home'}
+            as={Link}
+            to="/"
+            onClick={this.navbarButtonClicked}
+            position="right"
+            value="home-section"
+            style={{ fontFamily: `${fonts.main}, sans-serif` }}
+          >
+            Home
+          </Menu.Item>
+          <Menu.Item
+            name="profile"
+            active={activeMenuItem === 'profile'}
+            as={Link}
+            to="/profile"
+            onClick={this.navbarButtonClicked}
+            position="right"
+            value="profile-section"
+            style={{ fontFamily: `${fonts.main}, sans-serif` }}
+          >
+            Profile
+          </Menu.Item>
+          <Menu.Item
+            name="about"
+            active={activeMenuItem === 'about'}
+            as={Link}
+            to="/about"
+            onClick={this.navbarButtonClicked}
+            value="about-section"
+            style={{ fontFamily: `${fonts.main}, sans-serif`, cursor: 'pointer' }}
+          >
+            About
+          </Menu.Item>
+          <Menu.Item
+            name="signup"
+            active={activeMenuItem === 'signup'}
+            as={Link}
+            to="signup"
+            onClick={this.navbarButtonClicked}
+            value="signup-section"
+            style={{ fontFamily: `${fonts.main}, sans-serif`, cursor: 'pointer' }}
+          >
+            Sign Up
+          </Menu.Item>
+          <Menu.Item
+            name="login"
+            active={activeMenuItem === 'login'}
+            as={Link}
+            to="login"
+            onClick={this.navbarButtonClicked}
+            value="login-section"
+            style={{ fontFamily: `${fonts.main}, sans-serif`, cursor: 'pointer' }}
+          >
+            Login
+          </Menu.Item>
+        </Menu>
+      </Container>
+    );
+  }
+}
